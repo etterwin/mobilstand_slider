919fd3b597622b2f04f1fb2c45efade380960dd3
Dasha <etterwin.yll@gmail.com>
2018-08-13 12:57:38 +0300

    update

---
+++ fade/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    let detail_slider = new Swiper('.detail__container', {
+        effect: 'fade',
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+    });
+
+});
