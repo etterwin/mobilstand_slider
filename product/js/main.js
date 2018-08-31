4bc6c16fb858fd9a82ba6274d3c187ec020d9d1c
Dasha <etterwin.yll@gmail.com>
2018-08-31 10:44:30 +0300

    upload

---
+++ product/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    let productSlider = new Swiper('.product-slider__container', {
+        slidesPerView: 1,
+        loop: true,
+        grabCursor: true,
+        keyboardControl: true,
+        nextButton: '.product-slider-arrow.next',
+        prevButton: '.product-slider-arrow.prev',
+        pagination: '.product-slider-pagination',
+        paginationClickable: true,
+        autoplay: true,
+        speed: 1000,
+    });
+
+});
