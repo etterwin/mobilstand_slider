f083705c61d9ca5e89f952ffd527467783c1ce50
Dasha <etterwin.yll@gmail.com>
2018-09-11 15:41:03 +0300

    update

---
+++ pdf/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    let pdfSlider = new Swiper('.pdf__container', {
+        nextButton: '.pdf-btn.next',
+        prevButton: '.pdf-btn.prev',
+        pagination: '.pdf-pagination',
+        paginationType: 'fraction',
+    })
+
+});
